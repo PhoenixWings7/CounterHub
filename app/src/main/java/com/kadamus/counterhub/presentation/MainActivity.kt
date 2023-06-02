961d5d337023f9815b76143a33eadb0f30ef5e54
Klaudia Adamus <adamus.klaudia.pasp@gmail.com>
2023-06-02 10:25:34 +0200

    feature: settings menu action

--- app/src/main/java/com/kadamus/counterhub/presentation/MainActivity.kt
+++ app/src/main/java/com/kadamus/counterhub/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kadamus.counterhub.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,7 +14,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -28,16 +32,19 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kadamus.counterhub.R
 import com.kadamus.counterhub.domain.model.Counter
 import com.kadamus.counterhub.presentation.counters.SimpleCounter
+import com.kadamus.counterhub.presentation.settings.SettingsActivity
 import com.kadamus.counterhub.presentation.ui.theme.CounterHubTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -174,8 +181,30 @@ fun AppBarActions(onAddNewCounter: () -> Unit) {
             contentDescription = stringResource(id = R.string.cd_add_counter)
         )
     }
-    IconButton(onClick = { /*TODO*/ }) {
-        Icon(imageVector = Icons.Outlined.MoreVert, contentDescription = "Menu")
+    val isDropdownShown = remember { mutableStateOf(false) }
+    IconButton(onClick = { isDropdownShown.value = isDropdownShown.value.not() }) {
+        Icon(
+            imageVector = Icons.Outlined.MoreVert,
+            contentDescription = stringResource(id = R.string.cd_expandable_menu)
+        )
+    }
+    if (isDropdownShown.value) {
+        val context = LocalContext.current
+        DropdownMenu(
+            expanded = isDropdownShown.value,
+            onDismissRequest = { isDropdownShown.value = false })
+        {
+            DropdownMenuItem(
+                text = { Text(text = stringResource(id = R.string.lbl_settings)) },
+                leadingIcon = {
+                    Icon(
+                        Icons.Outlined.Settings,
+                        stringResource(id = R.string.cd_settings)
+                    )
+                }, onClick = {
+                    context.startActivity(Intent(context, SettingsActivity::class.java))
+                })
+        }
     }
 }
 
